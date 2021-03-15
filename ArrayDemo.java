25e907583ba6979067179464d6f19e0d0c537c80
Sakshikumari29 <54056189+Sakshikumari29@users.noreply.github.com>
2021-03-15 18:24:49 +0530

    Arrays

---
+++ ArrayDemo.java
@@ -0,0 +1,44 @@
+package udemy;
+
+import java.util.Arrays;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int[] arr; //recommended
+ int arr2[];
+ 
+ arr=new int[10];  
+ System.out.println(arr.length);
+ System.out.println(arr[0]); //0
+ 
+ double[] arr3=new double[10];
+ System.out.println(arr3[0]);
+ 
+ boolean[] arr4=new boolean[10];
+ System.out.println(arr4[0]);
+//	System.out.println(arr4[10]);  //runtime error
+ 
+ int[] arr5= {3,2,1};
+ 
+ int[][]matrix= {
+	
+		 {1,2,3},
+		 {4,5,6}
+ };
+ System.out.println(matrix[0][2]);//[row][column]
+ 
+ 
+ int[][] matrix2=new int[10][];   //not instantiated; not created ;reference type
+ System.out.println(matrix2[0]);
+ int[][] matrix21=new int[10][10];
+ System.out.println(matrix21[0]);
+ 
+ 
+ System.out.println(Arrays.toString(arr5));
+ Arrays.sort(arr5);
+ System.out.println(Arrays.toString(arr5));
+	}
+
+}
